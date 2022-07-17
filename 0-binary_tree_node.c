23723c981edddd288c9d792fa9fee4be76bd9619
AxelBouvierM <axelbouvierm@gmail.com>
2022-07-17 15:20:04 +0000

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * 
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *tmp;
+
+    tmp = malloc(sizeof(binary_tree_t));
+    if (tmp == NULL)
+        return (NULL);
+    
+    tmp->parent = parent;
+    tmp->n = value;
+    tmp->left = NULL;
+    tmp->right = NULL;
+
+    return (tmp);
+}
