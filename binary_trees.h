105ddb153d8fb5f87fc367cc7bf7b20610b91680
AxelBouvierM <axelbouvierm@gmail.com>
2022-07-17 15:25:22 +0000

    Prototypes and struct

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,7 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
-#include stdio.h
-#include stdlib.h
+#include "stdio.h"
+#include "stdlib.h"
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,10 +13,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -30,5 +30,6 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
